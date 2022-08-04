0aa615c1ce8a37553c036ac838551f76a24d2f1f
galinasu23-meet <109904723+galinasu23-meet@users.noreply.github.com>
2022-08-04 13:03:08 +0300

    h
    idk what i changed but i finished

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -20,15 +20,20 @@ config = {
 
     "measurementId": "G-6T0048M311",
 
-    "databaseURL": "https://gali-2be0b-default-rtdb.europe-west1.firebasedatabase.app/"
+    "databaseURL": "https://gali-2be0b-default-rtdb.europe-west1.firebasedatabase.app"
 
 }
 
 firebase= pyrebase.initialize_app(config)
 auth = firebase.auth()
+db= firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
+
+
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -50,23 +55,33 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        username = request.form['username']
+        fullname = request.form['fullname']
+        bio = request.form['bio']
+
         try:
-           login_session['user'] = auth.create_user_with_email_and_password(email, password)
-           return redirect(url_for('add_tweet'))
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            user = {"email":email, "password": password, "username": username,"fullname": fullname, "bio": bio}
+            db.child("Users").child(login_session['user']['localId']).set(user)
+
         except:
-             error = "Authentication failed"
+            error = "Authentication failed"
     return render_template("signup.html")
 
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        try :
+            tweet ={"title": title, "text": text, "uid":login_session['user']['localId']}
+            db.child("Tweets").push(tweet)
+            return redirect(url_for("all_tweets"))
+        except:
+            print("Authentication failed")
     return render_template("add_tweet.html")
-
-
-@app.route('/yourpage', methods=['GET', 'POST'])
-def yourpage():
-    return render_template("yourpage.html")
 
 
 @app.route('/signout')
@@ -74,6 +89,12 @@ def signout():
     login_session['user'] = None
     auth.current_user = None
     return redirect(url_for('signin'))
+
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweets():
+    tweets= db.child("Tweets").get().val()
+    return render_template("tweets.html", tweets= tweets)
 
 
 
